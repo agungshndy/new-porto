4afa6f2086dafc6a1d5575396cfd8c531a23bb0a
Agung Shandy Utama <agung.utama@moji.co.id>
2024-10-24 10:32:22 +0700

    major updates

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -4,6 +4,10 @@ function Information() {
             <section>
                 <div className="container relative">
                     <h1>Information</h1>
+                    <div>
+                        <p>View my CV here!</p>
+                        <a href="#" className="btn">CV</a>
+                    </div>
                 </div>
             </section>
         </div>
