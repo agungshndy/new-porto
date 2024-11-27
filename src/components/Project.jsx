bf9da85677995f27db9d374b09870daf73de4fa1
Agung Shandy <Call Center@Shandy.emtekgroup.id>
2024-11-27 22:53:15 +0700

    projects update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -35,24 +35,17 @@ function Project(){
             />
 
             <div className="relative border border-gray-100 bg-white p-6">
-              <p className="text-gray-700">
-                $49.99
-                <span className="text-gray-400 line-through">$80</span>
-              </p>
 
               <h3 className="mt-1.5 text-lg font-medium text-gray-900">
                 Keeper App
               </h3>
 
               <p className="mt-1.5 line-clamp-3 text-gray-700">
-                My Todo List App Project that I&apos;ve been worked on for a week.
+                My Todo List App Project that I&apos;ve been worked on.
               </p>
 
               <form className="mt-4 flex gap-4">
-                <button className="block w-full rounded bg-gray-100 px-4 py-3 text-sm font-medium text-gray-900 transition hover:scale-105">
-                  Add to Cart
-                </button>
-
+                
                 <button
                   type="button"
                   className="block w-full rounded bg-gray-900 px-4 py-3 text-sm font-medium text-white transition hover:scale-105"
